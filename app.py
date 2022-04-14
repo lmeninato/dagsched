0e5f37d01976233dea787b7efa996ffce407f41d
lmeninato <lorenzomeninato@gmail.com>
2022-04-14 16:27:51 -0400

    Working version using data stores clientside

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from src.dag import DAG
-from src.read_graph import read_dag_specs, parse_contents
+from src.read_graph import read_dag_specs, parse_contents, read_yaml
 
-import dash
+# import dash
 from dash import dcc
-from dash.dependencies import Input, Output, State
+
+import dash_daq as daq
 from dash import html
+from dash_extensions.enrich import DashProxy, MultiplexerTransform, Input, Output, State
+
 import dash_cytoscape as cyto
 import logging
+import glob
 
 logging.basicConfig(
     format="%(asctime)s %(levelname)s: %(message)s",
-    datefmt="%m/%d/%Y %I:%M:%S %p",
+    datefmt="%Y-%m-%d %I:%M:%S %p",
     level=logging.DEBUG,
 )
 
-app = dash.Dash(__name__)
+# app = dash.Dash(__name__)
+app = DashProxy(transforms=[MultiplexerTransform()])
 app.config.suppress_callback_exceptions = True
 
 cyto.load_extra_layouts()
@@ -24,74 +29,144 @@ specs = read_dag_specs("data")
 app.layout = html.Div(
     [
         html.P("DAG Scheduling Visualization"),
-        dcc.Upload(
-            id="upload-scheduling-spec",
-            children=html.Div(["Drag and Drop or ", html.A("Select Files")]),
-            style={
-                "width": "25%",
-                # "height": "60px",
-                # "lineHeight": "60px",
-                "borderWidth": "1px",
-                "borderStyle": "dashed",
-                "borderRadius": "5px",
-                "textAlign": "center",
-                "margin": "10px",
-            },
-            # Allow multiple files to be uploaded
-            multiple=True,
-        ),
-        dcc.Dropdown(
-            options=list(specs.keys()),
-            value=list(specs.keys())[0],
-            id="input-file-dropdown",
-        ),
-        dcc.Dropdown(
-            options=list(specs["simple_dag.yml"]["users"].keys()),
-            value=list(specs["simple_dag.yml"]["users"].keys())[0],
-            id="user-dropdown",
-        ),
-        cyto.Cytoscape(
-            id="cytoscape-elements-callbacks",
-            layout={
-                "name": "dagre",
-                # "animate": True,
-                # "animationDuration": 1000,
-                "rankDir": "LR",
-            },
-            autoRefreshLayout=True,
-            stylesheet=[
-                {
-                    "selector": "node",
-                    "style": {
-                        "background-color": "BFD7B5",
-                        "label": "data(label)",
-                        "text-wrap": "wrap",
-                        "text-valign": "bottom",
-                        "text-halign": "center",
-                    },
-                },
-                {"selector": "edge", "style": {"line-color": "#A3C4BC"}},
+        # store current list of DAGs
+        dcc.Store(id="session-cluster", storage_type="session"),
+        dcc.Store(id="session-dags", storage_type="session"),
+        dcc.Store(id="session-users", storage_type="session"),
+        dcc.Tabs(
+            id="tabs",
+            children=[
+                dcc.Tab(
+                    label="Input Scheduling Task",
+                    children=[
+                        dcc.Dropdown(
+                            id="input-dropdown",
+                            options=["Sample Tasks", "Import from File", "Custom"],
+                            value=["Sample Tasks"],
+                        ),
+                        html.Div(id="input-ui"),
+                    ],
+                ),
+                dcc.Tab(
+                    label="Scheduling Visualization",
+                    children=[
+                        dcc.Dropdown(id="user-dropdown"),
+                        cyto.Cytoscape(
+                            id="cytoscape-elements-callbacks",
+                            layout={
+                                "name": "dagre",
+                                # "animate": True,
+                                # "animationDuration": 1000,
+                                "rankDir": "LR",
+                            },
+                            autoRefreshLayout=True,
+                            stylesheet=[
+                                {
+                                    "selector": "node",
+                                    "style": {
+                                        "background-color": "BFD7B5",
+                                        "label": "data(label)",
+                                        "text-wrap": "wrap",
+                                        "text-valign": "bottom",
+                                        "text-halign": "center",
+                                    },
+                                },
+                                {
+                                    "selector": "edge",
+                                    "style": {"line-color": "#A3C4BC"},
+                                },
+                            ],
+                            style={
+                                "width": "50vw",
+                                "height": "75vh",
+                                "font-size": 8,
+                                "margin": "25px",
+                                "border": "grey solid",
+                            },
+                            elements=[],
+                        ),
+                    ],
+                ),
             ],
-            style={
-                "width": "50vw",
-                "height": "75vh",
-                "font-size": 8,
-                "margin": "25px",
-                "border": "grey solid",
-            },
-            elements=[],
         ),
     ]
 )
 
 
+@app.callback(Output("input-ui", "children"), [Input("input-dropdown", "value")])
+def render_input_ui(value):
+    input_option = value[0]
+    if input_option == "Import from File":
+        # "Import from File" is checked -> render ui to import file:
+        return [
+            dcc.Upload(
+                id="upload-scheduling-spec",
+                children=html.Div(["Drag and Drop or ", html.A("Select Files")]),
+                style={
+                    "width": "25%",
+                    # "height": "60px",
+                    # "lineHeight": "60px",
+                    "borderWidth": "1px",
+                    "borderStyle": "dashed",
+                    "borderRadius": "5px",
+                    "textAlign": "center",
+                    "margin": "10px",
+                },
+                # Allow multiple files to be uploaded
+                multiple=True,
+            ),
+        ]
+
+    elif input_option == "Sample Tasks":
+        return [
+            dcc.Dropdown(
+                id="sample-file-dropdown",
+                options=glob.glob("data/*.yml"),
+            ),
+        ]
+    # else create custom scheduling tasks data
+    return [
+        html.H1("todo - render UI for custom DAG"),
+        daq.NumericInput(id="input-number-users", value=1),
+        html.Div(id="custom-user-tasks-ui"),
+    ]
+
+
 @app.callback(
-    Output("input-file-dropdown", "options"),
+    Output("custom-user-tasks-ui", "children"), Input("input-number-users", "value")
+)
+def render_custom_user_tasks_ui(num_users):
+    logging.info("todo: render custom user tasks UI")
+    pass
+
+
+@app.callback(
+    Output("user-dropdown", "options"),
+    Output("user-dropdown", "value"),
+    [Input("session-users", "modified_timestamp")],
+    State("session-users", "data"),
+)
+def get_users_in_session(time_stamp, data):
+    if data:
+        users = [user["name"] for user in data]
+        return users, users[0]
+    return None, None
+
+
+@app.callback(
+    Output("session-cluster", "data"),
+    Output("session-dags", "data"),
+    Output("session-users", "data"),
     Input("upload-scheduling-spec", "contents"),
     State("upload-scheduling-spec", "filename"),
     State("upload-scheduling-spec", "last_modified"),
 )
-def udpate_scheduling_specs(list_of_contents, list_of_names, list_of_dates):
+def handle_file_upload(list_of_contents, list_of_names, list_of_dates):
+    logging.info("handling upload")
+    cluster = {}
+    data = {}
+    users = []
+
     if list_of_contents is not None:
         parsed_input_files = [
             parse_contents(c, n, d)
@@ -100,24 +175,63 @@ def udpate_scheduling_specs(list_of_contents, list_of_names, list_of_dates):
 
         for filename, spec in parsed_input_files:
             if spec is not None:
-                if filename in specs:
-                    logging.error(f"Error: {filename} already exists")
-                else:
-                    logging.info(f"Storing {filename} to specs")
-                    specs[filename] = spec
+                logging.info(f"Storing {filename} to session-dags")
+                cluster = spec["cluster"]
+                for user, tasks in spec["users"].items():
+                    data[user] = DAG(tasks)
+                    name = data[user].name
+                    users.append({"user": user, "name": name})
             else:
-                print(f"Failed to parse {filename}")
+                logging.error(f"Failed to parse {filename}")
+    return cluster, data, users
 
-    return list(specs.keys())
+
+@app.callback(
+    Output("session-cluster", "data"),
+    Output("session-dags", "data"),
+    Output("session-users", "data"),
+    Input("sample-file-dropdown", "value"),
+)
+def update_scheduling_tasks_from_sample(path):
+    cluster = {}
+    data = {}
+    users = []
+
+    if not path:
+        return None, None, None
+
+    try:
+        raw_data = read_yaml(path)
+        cluster = raw_data["cluster"]
+        for user, tasks in raw_data["users"].items():
+            data[user] = DAG(tasks)
+            name = data[user].name
+            users.append({"user": user, "name": name})
+    except Exception as e:
+        logging.error(f"error: {e}")
+        logging.error(f"Error reading sample file {path}")
+
+    return cluster, data, users
 
 
 @app.callback(
     Output("cytoscape-elements-callbacks", "elements"),
-    [Input("input-file-dropdown", "value"), Input("user-dropdown", "value")],
+    Input("user-dropdown", "value"),
+    State("session-dags", "data"),
+    State("session-users", "data"),
 )
-def update_shown_dag(input_file, input_user):
-    user_tasks = specs[input_file]["users"][input_user]
-    dag = DAG(user_tasks)
+def update_shown_dag(value, dags, users):
+    index = None
+    if not users:
+        return []
+    for i, user in enumerate(users):
+        if user["name"] == value:
+            index = i
+            break
+    if index is None:
+        return []
+    user = users[index]["user"]
+    dag = DAG(dags[user], deserialize=True)
     return dag.render_state()
 
 
--- src/dag.py
+++ src/dag.py
@@ -1,16 +1,17 @@
-import networkx as nx
-from networkx.drawing.nx_agraph import graphviz_layout
-from networkx.readwrite.json_graph.cytoscape import cytoscape_data
+import orjson
+from dataclasses import dataclass
 
 
+@dataclass
 class Task:
     required = ["label", "duration"]
     optional = {"cpus": 1, "ram": 1}
 
-    def __init__(self, props):
+    def __init__(self, name, props):
         """
         Build task
         """
+        self.id = name
         self.validate(props)
         self.props = self.add_defaults(props)
 
@@ -23,52 +24,57 @@ class Task:
         for opt, val in self.optional.items():
             if opt not in props:
                 props[opt] = val
+        props["id"] = self.id
         return props
 
     def get_props(self):
         return self.props
 
+    def toJSON(self):
+        return orjson.dumps(self)
 
+
+@dataclass
 class DAG:
     layout = None
 
-    def __init__(self, dag):
-        self.graph = nx.DiGraph()
+    def __init__(self, dag, deserialize=False):
+        self.nodes = []
+        self.edges = []
         self.name = dag["name"]
+        self.arrival_time = dag["arrival_time"]
 
+        if deserialize:
+            self.nodes = dag["nodes"]
+            self.edges = dag["edges"]
+            return
         for name, task in dag["tasks"].items():
-            task = Task(task)
-            task_props = task.get_props()
-            self.graph.add_node(name, **task_props)
-            if "dependencies" in task_props:
-                for dependency in task_props["dependencies"]:
-                    self.graph.add_edge(dependency, name, color="black")
+            self.add_task(name, task)
 
-        self.layout = graphviz_layout(self.graph)
+    def add_task(self, name, task):
+        task = Task(name, task)
+        props = task.get_props()
+        node = {"data": props}
+        self.nodes.append(node)
 
-    def to_cyto_node(self, node):
-        id = node["data"]["id"]
-        x, y = self.layout[id]
-        node["data"]["x"] = x
-        node["data"]["y"] = y
-        return node
+        if "dependencies" in props:
+            for dependency in props["dependencies"]:
+                edge = {"data": {"source": dependency, "target": name}}
+                self.edges.append(edge)
 
     def render_state(self):
         """
         Given events that have taken place, render current graph
         """
-        cyto = cytoscape_data(self.graph)
-        nodes = cyto["elements"]["nodes"]
-        edges = cyto["elements"]["edges"]
+        return self.nodes + self.edges
 
-        nodes = [self.to_cyto_node(node) for node in nodes]
-
-        return nodes + edges
+    def toJSON(self):
+        return orjson.dumps(self)
 
 
 if __name__ == "__main__":
     from read_graph import read_yaml
 
-    data = read_yaml("data/simple_dag.yaml")
+    data = read_yaml("data/simple_dag.yml")
     tasks = data["users"]["test_user"]
     dag = DAG(tasks)
