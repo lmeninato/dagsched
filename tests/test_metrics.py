393b1fcf53bfe39648c184d023f8a321c229402e
lmeninato <lorenzomeninato@gmail.com>
2022-04-25 15:05:03 -0400

    Add scheduling metrics class to scheduler

---
+++ tests/test_metrics.py
@@ -0,0 +1,37 @@
+import unittest
+from src.read_graph import read_yaml
+from src.scheduling import FCFS
+
+
+class TestSchedulingMetrics(unittest.TestCase):
+    """
+    pretty dumb tests for now, mostly just a sanity check
+    """
+
+    def setUp(self):
+        data = read_yaml("data/simple_dag.yml")
+        users = list(data["users"].keys())
+        scheduler = FCFS(data["cluster"], data["users"], users, deserialize=False)
+        scheduler.run()
+        self.metrics = scheduler.metrics
+
+    def test_makespan(self):
+        ms = self.metrics.get_makespan()
+        self.assertGreater(ms, 0)
+
+        ms = self.metrics.get_local_makespan("test_user")
+        self.assertGreater(ms, 0)
+
+    def test_jct(self):
+        ms = self.metrics.get_jct()
+        self.assertGreater(ms, 0)
+
+        ms = self.metrics.get_local_jct("test_user")
+        self.assertGreater(ms, 0)
+
+    def test_queuing_metrics(self):
+        ms = self.metrics.get_queuing_time()
+        self.assertGreaterEqual(ms, 0)
+
+        ms = self.metrics.get_local_queuing_time("test_user")
+        self.assertGreaterEqual(ms, 0)
